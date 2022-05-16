e3d8a42c80cf4eeb351329b56056b1e0e870741b
vladgba <vlad.gba@gmail.com>
2022-05-16 16:11:44 +0300

    added ip cache & minor improvements

--- geotrace.py
+++ geotrace.py
@@ -1,3 +1,5 @@
+import socket
+import sys
 import os
 import json
 from urllib.request import urlopen
@@ -8,11 +10,28 @@ def ipinfo(ip):
         return dict(json.loads(urlopen("http://ipwho.is/" + str(ip)).read()))
    except Except as e:
         return {"query":"".format(e), "success": false}
+
 def main():
+    save = False
+    mdir = os.path.dirname(os.path.realpath(__file__)) + "/"
     defip = "8.8.8.8"
-    tmpfile = "trace.tmp"
-    regex = r"(\d*)\s{1,}(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\*\s*$)\s*([\d\,\w]*|)"
-    site = input("target address (" + defip + "): ") or defip
+    cachefile = mdir + "ipcache.tmp"
+    tmpfile = mdir + "trace.tmp"
+    ipres = {}
+    if os.path.isfile(cachefile):
+        with open(cachefile) as f:
+            ipres = json.load(f)
+
+    regex = r"\n(\s+)?(\d+)\s+([0-9.]+|\*)"
+    site = ""
+    for i in range(1, len(sys.argv)):
+        if len(sys.argv[i]) < 5: continue
+        site = sys.argv[i]
+        break
+    if not site:
+        site = input("target address (" + defip + "): ") or defip
+    if not re.findall(regex,site):
+        site = socket.gethostbyname(site)
 
     os.system(('traceroute -n -q 1 {} > ' + tmpfile).format(site))
 
@@ -26,26 +45,33 @@ def main():
     items = re.findall(regex, trace)
 
     for item in items:
-        id = item[0]
-        ip = item[1]
+        id = item[1]
+        ip = item[2]
+
+        country = "--"
+        region = "-----"
+        city = "-----"
+        isp = "-----"
 
         if ip.strip() != "*":
-            info = ipinfo(ip)
+            if ip in ipres.keys():
+                info = ipres[ip]
+            else:
+                save = True
+                info = ipinfo(ip)
+                ipres[ip] = info
 
             if info['success']:
                 country = info["country_code"]
                 region = info["region"]
                 city = info["city"]
                 isp = info["connection"]["isp"]
-            else:
-                country = "**"
-                region = "***"
-                city = "***"
-                isp = "***"
 
-            print("{} {} {} {} {} {}".format("{0:2}".format(id), "{0:<15}".format(ip), "{0:2}".format(country), "{0:10}".format(region)[0:10], "{0:12}".format(city)[0:12], "{0:14}".format(isp)[0:14]))
-        else:
-            print("{} {} ** ***********".format("{0:2}".format(id), "{0:<15}".format(ip)))
+        print("{} {} {} {} {} {}".format("{0:2}".format(id), "{0:15}".format(ip), "{0:2}".format(country), "{0:10}".format(region)[0:10], "{0:12}".format(city)[0:12], "{0:14}".format(isp)[0:14]))
+
+    if save:
+        with open(cachefile, 'w') as f:
+            f.write(json.dumps(ipres))
 
 if __name__ == "__main__":
     main()
